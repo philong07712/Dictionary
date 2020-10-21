9c181b56edb8250c1aca258e25b2182a98650916
Han <philong07712@gmail.com>
2020-10-21 10:45:35 +0700

    + definition fragment

--- app/src/main/java/com/example/dictionary/view/DefinitionFragment.java
+++ app/src/main/java/com/example/dictionary/view/DefinitionFragment.java
@@ -8,27 +8,29 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Html;
+import android.text.SpannableString;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.dictionary.R;
+import com.example.dictionary.databinding.DefinitionFragmentBinding;
 import com.example.dictionary.model.Word;
 import com.example.dictionary.util.Constants;
 
 public class DefinitionFragment extends Fragment {
     private final String TAG = DefinitionFragment.class.getSimpleName();
     private DefinitionViewModel mViewModel;
+    private DefinitionFragmentBinding binding;
 
-    public static DefinitionFragment newInstance() {
-        return new DefinitionFragment();
-    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.definition_fragment, container, false);
+        binding = DefinitionFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
@@ -38,9 +40,15 @@ public class DefinitionFragment extends Fragment {
         Bundle bundle = this.getArguments();
         if (bundle != null) {
             Word word = (Word) bundle.getSerializable(Constants.WORD.WORD_ID);
-            Log.i(TAG, "onActivityCreated: " + word.getContent());
+            initDefinition(word);
         }
         // TODO: Use the ViewModel
+    }
+
+    public void initDefinition(Word word) {
+        binding.tvContent.setText(word.getContent());
+        SpannableString spannableString = new SpannableString(Html.fromHtml(word.getDefinition()));
+        binding.tvDescription.setText(spannableString);
     }
 
 }
